44e1fa362f55eb8471cefd39193454a70d930e3d
shuaijiezhang <zsj19941223@gamil.com>
2022-03-02 21:54:34 +0800

    feat: 3-2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld
+      msg="Welcome to Your Vue.js + TypeScript App"
+      v-model="name"
+      ref="son"
+    />
+    <p>your name: {{ name }}</p>
+    <button @click="useSon">点击</button>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue"
-  import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
+  import { defineComponent, ref } from 'vue'
+  import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
   export default defineComponent({
-    name: "Home",
+    name: 'Home',
     components: {
       HelloWorld,
+    },
+    setup() {
+      const name = ref('')
+      const son = ref<InstanceType<typeof HelloWorld>>()
+      function useSon() {
+        const sonFunc = son.value?.$.exposed?.sonFunc
+        if (typeof sonFunc === 'function') sonFunc()
+      }
+      const zsj = { name: 'zsj', age: 18 }
+      Object.keys(zsj).forEach((key) => {
+        console.log(key)
+      })
+
+      return {
+        name,
+        useSon,
+        son,
+      }
     },
   })
 </script>
